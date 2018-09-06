ee2ca7dcb0f2741c8223f98414b9e9a410d52469
Preston McNabb <prestonmcnabb22@gmail.com>
2018-09-06 18:25:57 -0500

    Added tests for anchor, link and new window

--- test/frontend/components/Navbar.test.js
+++ test/frontend/components/Navbar.test.js
@@ -1,7 +1,8 @@
 /* eslint-disable react/jsx-indent */
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Navbar from '../../../src/components/Navbar';
+import logo from '../../static/Moove_It-Logo_W.png';
 
 describe('Navbar', () => {
   it('renders without crashing', () => {
@@ -11,4 +12,21 @@ describe('Navbar', () => {
 
     mount(<Navbar />);
   });
+
+  it('contains an <a/> anchor element wrapped around <img/> logo', () => {
+  	const wrapper = shallow(Navbar />);
+  	expect(wrapper.find('a').contains(<img className="mooveItNavybg pt4 mt2 h1" src={logo} alt="Moove-it logo" />)).toEqual(true);
+
+  });
+
+  it('contains the correct href link', () => {
+  	const wrapper = shallow(Navbar />);
+  	expect(wrapper.find('a').prop('href')).toEqual('https://www.moove-it.com');
+  });
+
+  it ('contains the correct target _blank to open new window', () => {
+  	const wrapper = shallow(Navbar />);
+  	expect(wrapper.find('a').prop('target')).toEqual('_blank');
+  });
+
 });
